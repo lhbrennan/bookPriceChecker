62c8095f8c5193970a1f6b6bba99abd3f4f00f92
Luke Brennan <lhbrennan@gmail.com>
2018-10-29 13:25:47 -0700

    implement working main function

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
 const cTable = require('console.table'); // eslint-disable-line
 const { getBooks, addLinks } = require('./airtable.js');
-const { getAllPricesFromUrls } = require('./scrapeAmazon.js');
+const { getAllPricesFromUrls } = require('./getPricesFromAmazon.js');
 const addLinksFromAmazon = require('./addLinksFromAmazon.js');
 const mailer = require('./mailer');
 
 (async () => {
   const startTime = Date.now();
   let books = await getBooks();
+
   const booksWithoutLinks = books.filter(book => !book.link);
   if (booksWithoutLinks.length > 0) {
-    const booksWithNewLinks = await addLinksFromAmazon(booksWithoutLinks.slice(11, 14)); // remove for production
+    const booksWithNewLinks = await addLinksFromAmazon(booksWithoutLinks);
     addLinks(booksWithNewLinks);
     books = books.filter(book => book.link);
     books = [...books, booksWithNewLinks];
   }
 
-  // const scrapedKindleUrls = await getAllUrlsFromTitles(titlesNeedingUrls);
-  // const storedKindleUrls = await getKindleUrls();
-  // const allKindleUrls = [...storedKindleUrls, ...scrapedKindleUrls];
-  // const books = await getAllPricesFromUrls(allKindleUrls);
-  // console.table(books.map(book => ({ title: book.title, price: book.price })));
-  // const booksOnSale = books.filter(book => book.price < 3);
-  // if (booksOnSale[0]) {
-  //   mailer(booksOnSale);
-  // }
+  const booksWithPrices = await getAllPricesFromUrls(books);
+  console.table(booksWithPrices.map(book => ({ title: book.title, price: book.price })));
+  const booksOnSale = books.filter(book => book.price < 3);
+  if (booksOnSale[0]) {
+    mailer(booksOnSale);
+  }
   console.log('Runtime in seconds: ', (Date.now() - startTime) / 1000);
 })();
---
+++ getPricesFromAmazon.js
@@ -0,0 +1,69 @@
+const puppeteer = require('puppeteer');
+
+const headless = process.env.headless !== 'no';
+const slowMo = process.env.slowMo || 0;
+
+console.log('headless = ', headless);
+
+/* eslint-disable */
+const priceSelector = '#buybox > div > table > tbody > tr.kindle-price > td.a-color-price.a-size-medium.a-align-bottom';
+const priceSelectorAlt = '#mediaNoAccordion > div.a-row > div.a-column.a-span4.a-text-right.a-span-last > span.a-size-medium.a-color-price.header-price';
+/* eslint-enable */
+
+const getPriceByUrl = async function (link, browser) {
+  let price;
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1280, height: 800 });
+  await page.setRequestInterception(true);
+  page.on('request', (req) => {
+    if (req.resourceType() === 'image') {
+      req.abort();
+    } else {
+      req.continue();
+    }
+  });
+
+  try {
+    await page.goto(link, { waitFor: 'domcontentloaded' });
+    price = await page.evaluate((ps, psa) => {
+      const priceElement = document.querySelector(ps) || document.querySelector(psa);
+      return Number(priceElement.textContent.trim().split(' ')[0].slice(1));
+    }, priceSelector, priceSelectorAlt);
+  } catch (error) {
+    console.log('PROBLEM FETCHING PRICE FOR', link);
+  }
+  page.close();
+  return price || null;
+};
+
+async function getAllPricesFromUrls(books) {
+  const booksWithPrices = [...books];
+  const browser = await puppeteer.launch({
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+    ],
+    headless,
+    slowMo,
+    defaultViewport: { width: 1280, height: 800 },
+  });
+
+  await (async function () {
+    for (const book of booksWithPrices) {
+      try {
+        const price = await getPriceByUrl(book.link, browser);
+        console.log(`Found price for ${book.title}`);
+        book.price = price;
+      } catch (err) {
+        console.error(`**********\nFailed to find price for ${book.title}\n**********`, err);
+      }
+    }
+  }());
+  await browser.close();
+  return booksWithPrices;
+}
+
+module.exports = {
+  getAllPricesFromUrls,
+};
